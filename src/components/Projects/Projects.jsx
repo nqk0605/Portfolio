0601e1deb2c620284002a2faa32b83ecc1bb4062
nqk0605 <quockhang0605@gmail.com>
2025-02-04 20:20:26 +0700

    edit project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import ecShop from "../../Assets/Projects/ecshop.png";
+import sliderbook from "../../Assets/Projects/sliderbook.png";
 
 function Projects() {
   return (
@@ -30,6 +31,16 @@ function Projects() {
               description="MenWear is an online store specializing in men's fashion, offering trendy and modern styles with high-quality products. With the aim of providing customers with the best shopping experience, MenWear not only offers contemporary and stylish men's fashion items but also focuses on creating a user-friendly and innovative website interface."
               ghLink="https://github.com/nqk0605/ECShop"
               demoLink="https://menwear-shop.vercel.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={sliderbook}
+              isBlog={false}
+              title="SliderBook"
+              description="SliderBook is an online platform specializing in digital book sharing, offering a seamless and engaging reading experience. With the aim of making book access more convenient, SliderBook not only provides a diverse collection of books but also focuses on creating an interactive and user-friendly interface for readers."
+              ghLink="https://github.com/nqk0605/Book-Slider"
+              demoLink="https://bookslider.vercel.app/"
             />
           </Col>
         </Row>
